2bb641f98d4cd0605ae6341ba0e4832d7c2e5514
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-27 14:34:45 -0300

    feat: create controller get all restaurants

---
+++ src/restaurants/restaurants.controllers.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { paginationValidator } from '@/validators/paginationValidator';
+import { ZodError } from 'zod';
+import { getRestaurants } from './restaurants.services';
+import { mapIssuesZodError } from '@/utils/mapIssuesZodError';
+
+export function RestaurantsController() {
+	async function getAll(req: Request, res: Response) {
+		try {
+			const pagination = paginationValidator.parse(req.query);
+
+			const restaurants = await getRestaurants(pagination);
+			return res.status(200).json(restaurants);
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return res.status(400).json({ issues: mapIssuesZodError(error) });
+			}
+
+			if (error instanceof Error) {
+				return res.status(400).json({ message: error.message });
+			}
+
+			return res.status(500).json({ message: 'Erro interno no servidor' });
+		}
+	}
+
+	return {
+		getAll,
+	};
+}
