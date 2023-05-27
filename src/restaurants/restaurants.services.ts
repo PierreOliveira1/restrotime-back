05ddfb154bea5f54d952d1db5a880be1a87cb7d9
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-27 17:30:28 -0300

    feat: change Error to HTTPBadRequest

--- src/restaurants/restaurants.services.ts
+++ src/restaurants/restaurants.services.ts
@@ -2,15 +2,20 @@ import { prisma } from '@database';
 import { Prisma } from '@prisma/client';
 import { PaginationValidator } from '@/validators/paginationValidator';
 import { CacheManager } from '@/utils/cache';
+import { GetRestaurantsResponse } from './dto/getRestaurants.dto';
+import { HTTPBadRequestError } from '@/utils/httpBadRequest';
 
-async function getRestaurants({ page, limit }: PaginationValidator) {
+async function getRestaurants({ page, limit }: PaginationValidator): Promise<GetRestaurantsResponse> {
 	try {
 		const cache = CacheManager();
 		const cacheKey = `restaurants:${page}:${limit}`;
 
 		if (cache.has(cacheKey)) {
-			const cached = cache.get(cacheKey);
-			return cached;
+			const cached = cache.get<GetRestaurantsResponse>(cacheKey);
+
+			if (cached) {
+				return cached;
+			}
 		}
 
 		const restaurants = await prisma.restaurant.findMany({
@@ -26,7 +31,7 @@ async function getRestaurants({ page, limit }: PaginationValidator) {
 		const currentPage = page;
 		const nextPage = currentPage + 1 > totalPages ? null : currentPage + 1;
 
-		const data = {
+		const data: GetRestaurantsResponse = {
 			data: restaurants,
 			pagination: {
 				totalPages,
@@ -40,7 +45,7 @@ async function getRestaurants({ page, limit }: PaginationValidator) {
 		return data;
 	} catch (error) {
 		if (error instanceof Prisma.PrismaClientKnownRequestError) {
-			throw new Error('Erro ao buscar comércios');
+			throw new HTTPBadRequestError('Erro ao buscar restaurantes');
 		}
 
 		throw error;
