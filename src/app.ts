f466d2682c626093a864a0afe27e7f52f95dc4b9
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-26 23:42:22 -0300

    feat: app express

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import { createSwaggerDocument } from '@config/swagger';
+import { Routes } from './routes';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', Routes);
+
+createSwaggerDocument().then((swaggerDocument) => {
+	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+});
+
+export { app };
