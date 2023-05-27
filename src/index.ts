44705e04fd14878c3e0da8428fcb81b0a7859a86
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-26 23:42:34 -0300

    feat: server

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from 'node:http';
+import { app } from './app';
+import { PORT } from '@config/envs';
+
+const server = createServer(app);
+
+server.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
