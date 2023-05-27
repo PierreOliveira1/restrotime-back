fbf33f8853ddb8ed59a3752005864dce0b03de3a
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-27 14:36:35 -0300

    feat: change path from createSwaggerDocument

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
-import { createSwaggerDocument } from '@config/swagger';
+import { createSwaggerDocument } from '@/config/swagger';
 import { Routes } from './routes';
 
 const app = express();
