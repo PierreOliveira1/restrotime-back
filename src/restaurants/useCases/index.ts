07e412b88126702c1758d6236c6ea2f66516ceeb
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-28 18:24:47 -0300

    feat: export all use cases from restaurants

---
+++ src/restaurants/useCases/index.ts
@@ -0,0 +1,13 @@
+import { createRestaurantUseCase } from './createRestaurantUseCase';
+import { getRestaurantsUseCase } from './getRestaurantsUseCase';
+import { getRestaurantByIdUseCase } from './getRestaurantByIdUseCase';
+import { updateRestaurantByIdUseCase } from './updateRestaurantByIdUseCase';
+import { deleteRestaurantByIdUseCase } from './deleteRestaurantByIdUseCase';
+
+export {
+	createRestaurantUseCase,
+	getRestaurantsUseCase,
+	getRestaurantByIdUseCase,
+	updateRestaurantByIdUseCase,
+	deleteRestaurantByIdUseCase,
+};
