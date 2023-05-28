acb54bcb81647fefc30c527787fd4c40962b7e9c
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-27 21:41:20 -0300

    feat: add messages errors

--- src/validators/paginationValidator/index.ts
+++ src/validators/paginationValidator/index.ts
@@ -1,8 +1,18 @@
 import zod from 'zod';
 
 const paginationValidator = zod.object({
-	page: zod.number().min(1).default(1),
-	limit: zod.number().min(1).default(10),
+	page: zod
+		.number()
+		.min(1, {
+			message: 'A página deve ser maior que 0',
+		})
+		.default(1),
+	limit: zod
+		.number()
+		.min(1, {
+			message: 'O limite deve ser maior que 0',
+		})
+		.default(10),
 });
 
 type PaginationValidator = zod.infer<typeof paginationValidator>;
