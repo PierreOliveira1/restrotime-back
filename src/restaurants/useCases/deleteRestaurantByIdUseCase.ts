e9356006af6869e3e728713f1d123317c041bd80
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-28 18:24:08 -0300

    feat: create use case to delete restaurant

---
+++ src/restaurants/useCases/deleteRestaurantByIdUseCase.ts
@@ -0,0 +1,57 @@
+import { prisma } from '@database';
+import { CacheManager } from '@/utils/cache';
+import { HTTPRequestError } from '@/utils/httpRequestError';
+import { Prisma } from '@prisma/client';
+
+const cache = CacheManager();
+
+export async function deleteRestaurantByIdUseCase(id: string) {
+	try {
+		const restaurantExists = await prisma.restaurant.findUnique({
+			where: {
+				id,
+			},
+		});
+
+		if (!restaurantExists) {
+			throw new HTTPRequestError('Restaurante não encontrado', 404);
+		}
+
+		const deleteRestaurant = prisma.restaurant.delete({
+			where: {
+				id,
+			},
+		});
+
+		const deleteAddress = prisma.address.delete({
+			where: {
+				restaurantId: id,
+			},
+		});
+
+		const deleteSchedules = prisma.schedule.deleteMany({
+			where: {
+				restaurantId: id,
+			},
+		});
+
+		const [, , restaurant] = await prisma.$transaction([
+			deleteAddress,
+			deleteSchedules,
+			deleteRestaurant,
+		]);
+
+		if (cache.has(`restaurant:${restaurant.id}`)) {
+			cache.del(`restaurant:${restaurant.id}`);
+		}
+		cache.delMatch('^restaurants:');
+
+		return restaurant;
+	} catch (error) {
+		if (error instanceof Prisma.PrismaClientKnownRequestError) {
+			throw new HTTPRequestError('Erro ao deletar restaurante', 500);
+		}
+
+		throw error;
+	}
+}
