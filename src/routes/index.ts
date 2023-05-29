0894a9dbb50ed438bef1f755f8aab556eaf1c0cd
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-29 15:41:22 -0300

    feat: configured schedules in routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { Restaurants } from '../restaurants';
+import { Restaurants } from '@/restaurants';
+import { Schedules } from '@/schedules';
 
 const Routes = Router();
 
 Routes.use('/restaurants', Restaurants);
+Routes.use('/restaurants', Schedules);
 
 export { Routes };
