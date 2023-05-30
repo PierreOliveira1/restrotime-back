ff8947cfeb117701c240d0b9bf839b6c0e96842e
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-29 23:21:59 -0300

    feat: export all use cases of schedules

--- src/schedules/useCases/index.ts
+++ src/schedules/useCases/index.ts
@@ -1,3 +1,11 @@
 import { createSchedulesUseCase } from './createSchedulesUseCase';
+import { deleteSchedulesUseCase } from './deleteSchedulesUseCase';
+import { getSchedulesUseCase } from './getSchedulesUseCase';
+import { updateSchedulesUseCase } from './updateSchedulesUseCase';
 
-export { createSchedulesUseCase };
+export {
+	createSchedulesUseCase,
+	deleteSchedulesUseCase,
+	getSchedulesUseCase,
+	updateSchedulesUseCase,
+};
