c4753c0b30b693252a10d1d6cc253dfc92f83019
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-29 23:20:47 -0300

    feat: create all services to schedules

--- src/schedules/schedules.services.ts
+++ src/schedules/schedules.services.ts
@@ -1,6 +1,25 @@
 import { CreateSchedule } from './dto/createSchedule.dto';
-import { createSchedulesUseCase } from './useCases';
+import { DeleteSchedules } from './dto/deleteSchedules.dto';
+import { UpdateSchedules } from './dto/updateSchedules.dto';
+import {
+	createSchedulesUseCase,
+	deleteSchedulesUseCase,
+	getSchedulesUseCase,
+	updateSchedulesUseCase,
+} from './useCases';
 
 export function createSchedules(id: string, schedules: CreateSchedule) {
 	return createSchedulesUseCase(id, schedules);
+}
+
+export function getSchedules(id: string) {
+	return getSchedulesUseCase(id);
+}
+
+export function updateSchedules(id: string, schedules: UpdateSchedules) {
+	return updateSchedulesUseCase(id, schedules);
+}
+
+export function deleteSchedules(id: DeleteSchedules) {
+	return deleteSchedulesUseCase(id);
 }
