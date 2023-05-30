8d2a375bbb1a63cdaf415bde11eadede2f534f45
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-30 11:51:28 -0300

    feat: configured cors

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,18 @@ import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
 import { createSwaggerDocument } from '@/config/swagger';
 import { Routes } from './routes';
+import cors from 'cors';
+import { ORIGINS } from './config/envs';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+	cors({
+		origin: ORIGINS,
+	}),
+);
 app.use('/', Routes);
 
 createSwaggerDocument().then((swaggerDocument) => {
