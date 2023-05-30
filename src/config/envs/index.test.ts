033ca9fcca8e47e5aec083599ba270f0bed8cf9a
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-30 11:51:12 -0300

    feat: add env origins and test

--- src/config/envs/index.test.ts
+++ src/config/envs/index.test.ts
@@ -1,7 +1,11 @@
-import { PORT } from './index';
+import { ORIGINS, PORT } from './index';
 
 describe('Envs', () => {
 	it('Should error if the port is not a number', () => {
 		expect(isNaN(PORT)).toBe(false);
+	});
+
+	it('Should error if the port is not a number', () => {
+		expect(ORIGINS).toBeInstanceOf(Array);
 	});
 });
--- src/config/envs/index.ts
+++ src/config/envs/index.ts
@@ -3,3 +3,4 @@ import { config } from 'dotenv';
 config();
 
 export const PORT = Number(process.env.PORT);
+export const ORIGINS = process.env.ORIGINS.split(';');
