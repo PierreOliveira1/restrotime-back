d5c6fd749343b257e2fafa2cafa27fdfab49b987
Pierre Oliveira <pierre.torres445@gmail.com>
2023-05-30 22:07:28 -0300

    feat: create controller to search restaurant

--- src/restaurants/restaurants.controllers.ts
+++ src/restaurants/restaurants.controllers.ts
@@ -8,6 +8,7 @@ import {
 	updateRestaurantById,
 	deleteRestaurantById,
 	isOpenRestaurant,
+	searchRestaurant,
 } from './restaurants.services';
 import { mapIssuesZodError } from '@/utils/mapIssuesZodError';
 import { HTTPRequestError } from '@/utils/httpRequestError';
@@ -15,6 +16,7 @@ import { createRestaurantValidator } from './validators/createRestaurant';
 import { getRestaurantByIdValidator } from './validators/getRestaurantById';
 import { updateRestaurantValidator } from './validators/updateRestaurant';
 import { isOpenRestaurantValidator } from './validators/isOpenRestaurant';
+import { searchRestaurantValidator } from './validators/searchRestaurant';
 
 export function RestaurantsController() {
 	async function create(req: Request, res: Response) {
@@ -139,6 +141,29 @@ export function RestaurantsController() {
 		}
 	}
 
+	async function search(req: Request, res: Response) {
+		try {
+			const pagination = paginationValidator.parse({
+				page: Number(req.query.page),
+				limit: Number(req.query.limit),
+			});
+			const { search } = searchRestaurantValidator.parse(req.query);
+
+			const restaurants = await searchRestaurant(search, pagination);
+			return res.status(200).json(restaurants);
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return res.status(400).json({ issues: mapIssuesZodError(error) });
+			}
+
+			if (error instanceof HTTPRequestError) {
+				return res.status(error.statusCode).json({ message: error.message });
+			}
+
+			return res.status(500).json({ message: 'Erro interno no servidor' });
+		}
+	}
+
 	return {
 		create,
 		getAll,
@@ -146,5 +171,6 @@ export function RestaurantsController() {
 		updateById,
 		deleteById,
 		isOpen,
+		search,
 	};
 }
